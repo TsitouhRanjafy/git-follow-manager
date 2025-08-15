0d685ed69dc3516a068eafb1fc9dce6a9fedd813
TsitouhRanjafy <radohery09@gmail.com>
2025-08-15 12:15:35 +0300

    feat: affiche following

--- command/following.js
+++ command/following.js
@@ -1,26 +1,35 @@
+import { fetch_following } from "../github-api/subscribe.js";
+import { get_env } from "../helper/env.access.js";
+import chalk from "chalk";
+
 export default {
     command: 'following',
-    description: 'Affiche tout l\'utilisateur qui vous suit\n',
+    description: 'Affiche l\'utilisateur que vous avez suivie\n',
     builder: (yargs) => {
         return yargs
-         .option('all', {
-                alias: 'a',
-                type: 'boolean',
-                describe: "Afficher tous les utilisateurs"
-            })
             .option('limite', {
                 alias: 'l',
                 type: 'number',
-                describe: 'Limiter le nombre d\'utilisateurs affichés'
+                describe: 'Le nombre d\'utilisateurs à afficher',
+                default: 30
             })
-            .option('paging', {
+            .option('page', {
                 alias: 'p',
                 type: 'number',
-                describe: 'Indique le numéro de page pour la pagination'
+                describe: 'Indique le numéro de page',
+                default: 1,
             })
+            .example('$0 following -l 8 -p 1','Affiche le 8 utilisateur que vous avez suivie dans page 1')
+            .demandCommand(0)
     },
-    handler: (argv) => {
-      console.log('commend following executer avec ',argv);
+    handler: async (argv) => {
+        try {
+            const token = await get_env("my_token");
+            const list = await fetch_following(token, argv.limite, argv.page)
+            console.table(list);
+        } catch (error) {
+            console.error(chalk.hex('#FFA500')(" !Internal error, cmd: following"));
+        }
     },
     middleware: undefined
 }
