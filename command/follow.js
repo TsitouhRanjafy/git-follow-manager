83fc1235aa93420b8b672c7c2980a29f9c1ee7f4
TsitouhRanjafy <radohery09@gmail.com>
2025-08-15 23:13:23 +0300

    feat: follow and unfollow implementation

--- command/follow.js
+++ command/follow.js
@@ -1,3 +1,9 @@
+import chalk from "chalk";
+import { follow_or_unfollow_user } from "../github-api/user-action.js";
+import { get_env } from "../helper/env.access.js";
+import { readFileSync } from "node:fs";
+import ora from "ora";
+
 export default {
     command: 'follow',
     description: 'S\'abonner à utilisateur',
@@ -11,11 +17,50 @@ export default {
             .option('json', {
                 alias: 'j',
                 type: 'string',
-                describe: 'Indiqué le chemin vers un fichier JSON contenant les noms des utilisateurs à suivre'
+                describe: `Indiqué le chemin vers un fichier JSON contenant les noms des utilisateurs à suivre
+                Le format json doit être un tableau qui contient l'object { user_name: string }
+                `
+            })
+            .check((argv) => {
+                if ((!argv.username && !argv.json) || (argv.username && argv.json)) {
+                    throw new Error(chalk.hex('#FFA500')(" !Vous devez spécifier --username ou --json"));
+                }
+
+                return true
             })
     },
-    handler: (argv) => {
-      console.log('commend follow executer avec ',argv);
+    handler: async (argv) => {
+        try {
+            const token = await get_env("my_token");
+            
+            if (argv.json) {
+                follow_user_in_json(token, argv.json)
+                return
+            }
+
+            const status = await follow_or_unfollow_user(token, argv.username)
+            
+            if (status) {
+                console.info(chalk.green(` Utilisateur suivie avec succès: ${argv.username}`));
+            }
+            
+        } catch (error) {
+            console.error(chalk.hex('#FFA500')(` !Internal error, cmd: follow`))
+        }
     },
     middleware: undefined
+}
+
+async function follow_user_in_json(token, json_path) {
+    const spinner = ora().start()
+    try {
+        let data = readFileSync(json_path, 'utf-8');
+        data = JSON.parse(data);
+        for (const user of data) {
+            await follow_or_unfollow_user(token, user.user_name)
+        }
+        spinner.succeed(chalk.green(` Finished`))
+    } catch (error) {
+        spinner.fail(chalk.hex('#FFA500')(` !Internal error, Reference: follow_user_in_json\n  `, error.message))
+    }    
 }
---
+++ github-api/user-action.js
@@ -0,0 +1,39 @@
+import { base_url } from "../helper/env.access.js";
+import chalk from "chalk";
+
+const follow_or_unfollow_user = async (token, user_name, is_follow = true) => {
+    try {
+        const res = await fetch(`${base_url}/user/following/${user_name}`,{
+            method: is_follow ? 'PUT' : 'DELETE',
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${token}`,
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        })
+
+        if (res.status == 401) {
+            console.info(chalk.hex('#FFA500')(" !Vous n'est pas authentifier, execute: ./index.js login"));
+            return false
+        }
+
+        if (res.status == 304) {
+            console.info(chalk.hex('#FFA500')(` !Utilisateur déjà ${!is_follow ?? 'non'} suivie: ${user_name}`));
+            return false
+        }
+
+        if (res.status == 404) {
+            console.info(chalk.hex('#FFA500')(` !Utilisateur non trouvé: ${user_name}`));
+            return false
+        }
+
+        return true
+    } catch (error) {
+        console.error(chalk.hex('#FFA500')(` !Internal error, Reference: follow_or_unfollow_user`));
+        return false
+    }
+}
+
+export {
+    follow_or_unfollow_user
+}
